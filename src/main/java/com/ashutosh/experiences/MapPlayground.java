2fc7ff6932f78879ee7e00861b67a1eecb18ddf7
Ashr005 <as2733@sussex.ac.uk>
2026-07-31 18:11:33 +0100

    New

---
+++ src/main/java/com/ashutosh/experiences/MapPlayground.java
@@ -0,0 +1,23 @@
+package com.ashutosh.experiences;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPlayground {
+
+    public static void main(String[] args) {
+        String[] tours   = {"Punting", "Colosseum", "Punting", "Kayaking", "Punting"};
+        int[]    parties = {2,          4,           3,         1,          2};
+      Map<String, Integer> placesPerTour = new HashMap<>();
+
+for (int i = 0; i < tours.length; i++) {
+    
+    String new = tours[i];
+    int inew = parties[i]
+    
+    
+}
+    
+System.out.println(placesPerTour);
+}
+}
